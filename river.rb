9830dcc40e7775fe19521c07317a6b54bc382715
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-13 22:07:35 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,30 @@
-# river.rb
+class Rivers
+  attr_accessor :name, :length, :country, :discharge
+  def initialize(name, length, country, discharge)
+    
+@name = name
+@length = length
+@country = country
+@discharge = discharge
+@flood = flood
+end 
+end
+
+
+def flood
+  if flood == "dry up"
+  discharge1 = discharge*0.70
+  end
+end
+
+def flood2
+  if flood == "flood"
+  discharge1 = discharge*1.50
+  end
+end
+
+  
+the_nile=Rivers.new("The Nile", "5 feet", "USZA", "99,940" "flood")
+
+
+ puts the_nile.discharge
